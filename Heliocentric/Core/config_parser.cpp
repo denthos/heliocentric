9db1cadfadc33137aa1a174585417cd663b88b37
Ethan <Ethanleet@users.noreply.github.com>
2017-04-20 00:28:51 -0700

    [core] Change Lib into a namespace with config parser incorporated. (#37)
    * Incorporated config parser into Lib.
    
    * Removed original config parser files. Changed declaration of utility functions to non-static under Lib namespace.
    
    * Put config parser back to separate files.
    
    * Fixed inclusion in main.cpp
    
    * Added a Lib filter.
    
    * Fixed Lib filter.
    
    * Replaced custom distance() function with glm::distance().
    
    * Update project.

---
+++ Heliocentric/Core/config_parser.cpp
@@ -0,0 +1,62 @@
+#include "config_parser.h"
+
+namespace Lib {
+	bool ConfigParser::get_value(std::string key, bool& ret) {
+		std::string value;
+		bool exists = this->get_value(key, value);
+		if (exists) {
+			return (value == "true") ? true : false;
+		}
+		return exists;
+	}
+
+	bool ConfigParser::get_value(std::string key, int& ret) {
+		std::string value;
+		bool exists = this->get_value(key, value);
+		if (exists) {
+			ret = std::atoi(value.c_str());
+		}
+		return exists;
+	}
+
+	bool ConfigParser::get_value(std::string key, float& ret) {
+		std::string value;
+		bool exists = this->get_value(key, value);
+		if (exists) {
+			ret = (float)std::atof(value.c_str());
+		}
+		return exists;
+	}
+
+	bool ConfigParser::get_value(std::string key, double& ret) {
+		std::string value;
+		bool exists = this->get_value(key, value);
+		if (exists) {
+			ret = std::atof(value.c_str());
+		}
+		return exists;
+	}
+
+	void ConfigParser::update_value(std::string key, bool value) {
+		std::string v = (value) ? "true" : "false";
+		update_value(key, v);
+	}
+
+	void ConfigParser::update_value(std::string key, int value) {
+		std::stringstream s;
+		s << value;
+		update_value(key, s.str());
+	}
+
+	void ConfigParser::update_value(std::string key, float value) {
+		std::stringstream s;
+		s << value;
+		update_value(key, s.str());
+	}
+
+	void ConfigParser::update_value(std::string key, double value) {
+		std::stringstream s;
+		s << value;
+		update_value(key, s.str());
+	}
+}
