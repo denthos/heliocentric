1c97a79b113d3679178c1957f02f98ae0fa3a054
Brandon Milton <brandon.milton21@gmail.com>
2017-04-21 12:24:46 -0700

    Separate logging into h and cpp (#51)

--- Heliocentric/Core/logging.h
+++ Heliocentric/Core/logging.h
@@ -23,13 +23,7 @@ namespace Lib {
 #define LOG_WARN(...) LOG(LOG_LEVEL_WARN, __VA_ARGS__)
 #define LOG_ERR(...) LOG(LOG_LEVEL_ERR, __VA_ARGS__)
 
-	std::ofstream& getLogFile(std::string dir) {
-		static std::ofstream log_file;
-		if (!log_file.is_open()) {
-			log_file.open(dir + "/log.log", std::ios::app);
-		}
-		return log_file;
-	}
+	std::ofstream& getLogFile(std::string dir);
 
 	template <typename TStreamType>
 	void write_log_to_stream(TStreamType& out_stream, std::string& level_string, const char* file, const char* ext, int line, const char* time_str, std::ostringstream& stream) {
@@ -67,35 +61,7 @@ namespace Lib {
 		LogFuncRecursive(level, file, ext, dir, line, time_str, stream, args...);
 	}
 
-	void LogFuncRecursive(int level, const char* file, const char* ext, const char* dir, int line, const char* time_str, std::ostringstream& stream) {
-		std::string level_string;
-		switch (level) {
-		case LOG_LEVEL_DEBUG:
-			level_string = "DEBUG";
-			break;
-		case LOG_LEVEL_WARN:
-			level_string = "WARN";
-			break;
-		case LOG_LEVEL_ERR:
-			level_string = "ERROR";
-			break;
-		default:
-			level_string = "LOG";
-		}
+	void LogFuncRecursive(int level, const char* file, const char* ext, const char* dir, int line, const char* time_str, std::ostringstream& stream);
 
-		write_log_to_stream(std::cerr, level_string, file, ext, line, time_str, stream);
-		write_log_to_stream(getLogFile(dir), level_string, file, ext, line, time_str, stream);
-	}
-
-
-
-	void get_file_info(const char* path, char* filename, int filename_size, char* extension, int ext_size, char* directory, int dir_size) {
-#ifdef _WIN32
-		_splitpath_s(path, NULL, 0, directory, dir_size, filename, filename_size, extension, ext_size);
-#else
-		dirname_r(path, directory);
-		basename_r(path, filename);
-		memset(extension, 0, ext_size);
-#endif
-	}
+	void get_file_info(const char* path, char* filename, int filename_size, char* extension, int ext_size, char* directory, int dir_size);
 }
