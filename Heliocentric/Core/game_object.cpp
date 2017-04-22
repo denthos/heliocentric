60fdadc23e1831e331c53d440db6898becc6bd4a
Pritvi Raj Kumar <raj.pritvi.kumar@gmail.com>
2017-04-22 02:15:03 -0700

    Return reference on `GameObject::get_position()`

--- Heliocentric/Core/game_object.cpp
+++ Heliocentric/Core/game_object.cpp
@@ -10,6 +10,6 @@ void GameObject::set_player(Player* assigned_player) {
 	player = assigned_player;
 }
 
-glm::vec3 GameObject::get_position() {
+glm::vec3& GameObject::get_position() {
 	return this->position;
 }
