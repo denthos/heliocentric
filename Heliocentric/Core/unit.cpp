32ed859a0d5ea4ff2905ae385534417c205974dc
Pritvi Raj Kumar <raj.pritvi.kumar@gmail.com>
2017-04-23 12:53:23 -0700

    Unset unit's player upon death

--- Heliocentric/Core/unit.cpp
+++ Heliocentric/Core/unit.cpp
@@ -69,6 +69,7 @@ void Unit::handle_defeat(AttackableGameObject * opponent)
 {
 	// Tell Player you have died.
 	player->add_to_destroy(this);
+	player = NULL;
 }
 
 void Unit::handle_victory(AttackableGameObject * opponent)
