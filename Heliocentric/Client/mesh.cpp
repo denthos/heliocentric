d0ac3e9e5aa6f2f7ae53fdabfec7a56a66095d7f
Brandon Milton <brandon.milton21@gmail.com>
2017-04-25 11:01:39 -0700

    Remove two warnings by explicitly casting datatypes (#56)

--- Heliocentric/Client/mesh.cpp
+++ Heliocentric/Client/mesh.cpp
@@ -76,7 +76,7 @@ void Mesh::Draw(Shader &shader, const Camera & camera)
 	glBindVertexArray(VAO);
 
 	//draw
-	glDrawElements(GL_TRIANGLES, mesh_indices.size(), GL_UNSIGNED_INT, 0);
+	glDrawElements(GL_TRIANGLES, (GLsizei) mesh_indices.size(), GL_UNSIGNED_INT, 0);
 
 	glBindVertexArray(0);
 
