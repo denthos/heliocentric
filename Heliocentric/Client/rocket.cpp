5a874676ae7d27f01fbb833c659361c6a40a2350
JJ <jjtran.mail@gmail.com>
2017-05-04 13:39:18 -0700

    temporarily separating rocket from drawable unit so i can test

--- Heliocentric/Client/rocket.cpp
+++ Heliocentric/Client/rocket.cpp
@@ -1,16 +1,40 @@
 #include "rocket.h"
 
-Rocket::Rocket(Model & model)
+Rocket::Rocket(Model *model)
 {
-	rocketModel = model;
+	this->model = model;
+}
+
+Rocket::~Rocket()
+{
 }
 
 void Rocket::TurnTowards(glm::vec3 point)
 {
-	
-	glm::mat4 lookAt = glm::lookAt(pos, point, up);
-	glm::quat toRotQuat = glm::quat_cast( glm::transpose(lookAt)); //the final rotation quaternion
+	float time = 1.0f; //TODO
+	glm::mat4 lookAt = glm::lookAt(position, point, up);
+	glm::quat toRotQuat = glm::quat_cast(glm::transpose(lookAt)); //the final rotation quaternion
 
-	glm::quat rotQuat = glm::slerp( , toRotQuat);
+	//for smooth rotation
+	glm::quat rotQuat = glm::slerp(glm::quat_cast(rot_mat), toRotQuat, time);
+
+	//convert back to matrix, save and apply
+	rot_mat = glm::mat4_cast(rotQuat);
+
+}
+
+void Rocket::draw(const Shader & shader, const Camera & camera, const glm::mat4 & toWorld)
+{
+	model->draw(shader, camera, toWorld);
+}
+
+void Rocket::update(glm::vec3 point)
+{
+	if (turn) {
+		TurnTowards();
+
+
+	
+	}
 
 }
--- Heliocentric/Client/rocket.h
+++ Heliocentric/Client/rocket.h
@@ -1,23 +1,29 @@
 #pragma once
 #include "model.h"
+#include "drawable_unit.h"
 #include <glm\gtc\matrix_transform.hpp>
 #include <glm\gtc\quaternion.hpp>
 
-class Rocket { //TODO extend drawable unit
+class Rocket{ //TODO extend drawable unit
 public:
-	Rocket(Model &model);
+	Rocket(Model *model);
 	~Rocket();
 
 	//animation sequences
 	void TurnTowards(glm::vec3 point);
 	void ShootLaser();
 
-	
+	void draw(const Shader & shader, const Camera & camera, const glm::mat4 & toWorld);
+	void update(glm::vec3 point);
 
 private:
-	Model rocketModel;
+	Model *model; //TODO remove
 	glm::vec3 up = glm::vec3(0.0f, 1.0f, 0.0f); //up direction is always positive y axis
-	glm::vec3 pos; //this should be part of drawable unit class
+	glm::mat4 rot_mat = glm::mat4(1.0f);
+	glm::vec3 position = glm::vec3(0.0f); //TODO remove
+
+	bool turn = false;
+	
 	
 
 };
