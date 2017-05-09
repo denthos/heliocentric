f242152f6dff46613c5b5a066a17bcf7652d96f2
Brandon Milton <brandon.milton21@gmail.com>
2017-05-08 21:54:27 -0700

    Make UnitManager a tad more resilient (#132)

--- Heliocentric/Core/unit_manager.cpp
+++ Heliocentric/Core/unit_manager.cpp
@@ -7,15 +7,18 @@ UnitManager::UnitManager() {}
 
 void UnitManager::doLogic() {
 	this->unit_updates.clear();
+
 	auto& active_unit = active_units.begin();
 	while (active_unit != active_units.end()) {
 		Unit::CommandType command = active_unit->second->do_logic();
+
 		if (command == Unit::UNIT_IDLE) {
-			idle_units[active_unit->first] = std::move(active_unit->second);
-			active_units.erase(active_unit++);
-		} else {
+			idle_units.insert(std::make_pair(active_unit->first, std::move(active_unit->second)));
+			active_unit = active_units.erase(active_unit);
+		} 
+		else {
 			this->unit_updates.insert(active_unit->second->make_update());
-			++active_unit;
+			active_unit++;
 		}
 	}
 }
@@ -31,19 +34,28 @@ std::shared_ptr<UnitCreationUpdate> UnitManager::add_unit(std::shared_ptr<Player
 	auto update = std::make_shared<UnitCreationUpdate>(new_unit->getID(),
 		command->create_location_x, command->create_location_y, command->create_location_z,
 		player->getID(), 100, 100, 20, 100);
-	idle_units[new_unit->getID()] = std::move(new_unit);
+
+	idle_units.insert(std::make_pair(new_unit->getID(), std::move(new_unit)));
 	return update;
 }
 
 void UnitManager::do_move(UID id, float x, float y, float z) {
-	std::unordered_map<UID, std::unique_ptr<Unit>>::iterator itr = idle_units.find(id);
+	auto& itr = idle_units.find(id);
 	if (itr != idle_units.end()) {
-		active_units[id] = std::move(idle_units[id]);
-		idle_units.erase(id);
+		active_units.insert(std::make_pair(id, std::move(itr->second)));
+		idle_units.erase(itr);
 	}
+
 	LOG_DEBUG("Destination is set to " + std::to_string(x) + " " + std::to_string(y) + " " + std::to_string(z));
-	active_units[id]->set_destination(glm::vec3(x, y, z));
-	active_units[id]->set_command(Unit::UNIT_MOVE);
+
+	auto& active_units_iter = active_units.find(id);
+	if (active_units_iter == active_units.end()) {
+		LOG_ERR("Trying to move unit with id ", id, " but it could not be found...");
+		return;
+	}
+
+	active_units_iter->second->set_destination(glm::vec3(x, y, z));
+	active_units_iter->second->set_command(Unit::UNIT_MOVE);
 }
 
 
