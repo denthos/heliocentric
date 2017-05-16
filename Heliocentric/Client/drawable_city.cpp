860696bdf61dc8a95cdaf76094359766da67048b
Brandon Milton <brandon.milton21@gmail.com>
2017-05-15 18:09:00 -0700

    [Client] Add City Model and Rotate It (#142)
    * Add rotation for slots - give a model
    
    * It's working!
    
    * Adjust planet's toWorld with new learnings!
    
    * Remove 2 unnecessary includes
    
    * Add city model and rotation support
    
    * Resolve more merge stuff
    
    * Use GLM functions and default parameters

--- Heliocentric/Client/drawable_city.cpp
+++ Heliocentric/Client/drawable_city.cpp
@@ -2,11 +2,13 @@
 #include "drawable_planet.h"
 #include "sphere_model.h"
 
+#include <glm/gtx/transform.hpp>
+
 DrawableCity::DrawableCity(const City& city) : City(city) {
-	this->model = new SphereModel(Texture::getInstance("Textures/jupiter.jpg"));
-	this->toWorld = glm::translate(glm::mat4(1.0f), get_position());
+	this->model = new Model("Models/city.obj");
+	this->toWorld = glm::translate(get_slot()->get_absolute_position()) *  get_slot()->get_spherical_position().getRotationMatrix() * glm::scale(glm::vec3(get_slot()->getPlanet()->get_radius() / 10.0f));
 }
 
 void DrawableCity::update() {
-	this->toWorld = glm::translate(glm::mat4(1.0f), get_position());
+	this->toWorld = glm::translate(get_slot()->get_absolute_position()) *  get_slot()->get_spherical_position().getRotationMatrix() * glm::scale(glm::vec3(get_slot()->getPlanet()->get_radius() / 10.0f));
 }
