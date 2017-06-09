5953dbbba815dd0b5f518b083d2e57c7801700f8
Ethan Li <ethanleet@me.com>
2017-06-08 17:50:06 -0700

    Fix dependency issues

--- Heliocentric/Core/building.cpp
+++ Heliocentric/Core/building.cpp
@@ -1,4 +1,5 @@
 #include "building.h"
+#include "building_type.h"
 
 Building::Building(BuildingType* type) : production(type->getProduction()), research_points(type->getResearchPoints()) {
 
--- Heliocentric/Core/building.h
+++ Heliocentric/Core/building.h
@@ -1,6 +1,6 @@
 #pragma once
 
-#include "building_type.h"
+class BuildingType;
 
 class Building {
 public:
