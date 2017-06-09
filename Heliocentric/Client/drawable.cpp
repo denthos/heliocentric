7b05fd4ea9002e040844241600fadb569d8ea47d
Brandon Milton <brandon.milton21@gmail.com>
2017-06-09 11:33:33 -0700

    [Client] Revamp Updates, Add Proper Explosion and Laser Audio (#292)
    * Revamp drawable updates, update in sync, add working laser and explosion audio
    
    * Fix issue

--- Heliocentric/Client/drawable.cpp
+++ Heliocentric/Client/drawable.cpp
@@ -48,8 +48,18 @@ void init() {
 }
 #endif
 
-Drawable::Drawable() : toWorld(glm::mat4(1.0f)) {
+Drawable::Drawable() : toWorld(glm::mat4(1.0f)), needs_update(false) {
+}
 
+void Drawable::mark_for_update() {
+	needs_update = true;
+}
+
+void Drawable::performUpdate(bool force) {
+	if (needs_update || force) {
+		this->update();
+		needs_update = false;
+	}
 }
 
 void Drawable::draw(const Camera & camera) const {
--- Heliocentric/Client/drawable_city.h
+++ Heliocentric/Client/drawable_city.h
@@ -8,14 +8,18 @@
 class DrawableSlot;
 
 class DrawableCity : public City, public Drawable, public Selectable {
+	friend class DrawableSlot;
+
 private:
 	DrawableSlot* drawable_slot;
 	PlayerIcon* icon;
 
 public:
 	DrawableCity(const City&, Shader * shader, PlayerIcon * icon);
-	virtual void update();
 	virtual void draw(const Camera & camera) const;
 	void select(GUI* gui, Client* client);
 	void unselect(GUI* gui, Client* client);
+
+protected:
+	virtual void update();
 };
